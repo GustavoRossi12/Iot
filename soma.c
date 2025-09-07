2b77288a4c916919e616af010553abf88ecabdda
Gustavo_Rossi <gustavo.259523@alunosatc.edu.br>
2025-09-07 17:29:04 -0300

    Create soma.c

---
+++ soma.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+        int n1, s, n2;
+    printf("digite seu primeiro numero:\n");
+    scanf("%d", &n1);
+    
+    printf("digite seu segundo numero:\n");
+    scanf("%d", &n2);
+
+    s = n1 + n2;
+    
+    if (s > 10){
+        printf("seu valor é %d", s);
+    }
+
+
+    
+
+    
+}
